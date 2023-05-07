4a4072d2a7366ef4d1ba77f227ea886ce0dba8c9
masterpranay1 <rajpranaychottu@gmail.com>
2023-05-07 15:55:35 +0530

    project-setup

---
+++ server.js
@@ -0,0 +1,54 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import connectDB from './config/db.js';
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+connectDB();
+
+io.on('connection', (socket) => {
+  console.log("connected!!" + socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected' + socket.id);
+  })
+})
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+httpServer.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
