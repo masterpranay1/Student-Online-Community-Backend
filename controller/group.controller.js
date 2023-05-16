e3863768d5fd0cf334a86c284ca8273e335ba4c8
masterpranay1 <rajpranaychottu@gmail.com>
2023-05-16 10:25:49 +0530

    Update(Group and User Controller)

--- controller/group.controller.js
+++ controller/group.controller.js
@@ -1,113 +1,115 @@
-import expressAsyncHandler from "express-async-handler";
-import Admin from "../models/user.model.js";
-import User from "../models/user.model.js";
-import Channel from "../models/channel.model.js";
-import Group from "../models/group.model.js";
+import expressAsyncHandler from 'express-async-handler';
+import Admin from '../models/user.model.js';
+import User from '../models/user.model.js';
+import Channel from '../models/channel.model.js';
+import Group from '../models/group.model.js';
 
-// @desc Create a new group
-// @route POST /api/admin/createGroup or /api/moderator/createGroup
-// @access Private
+class GroupController {
+  // @desc Create a new group
+  // @route POST /api/admin/createGroup or /api/moderator/createGroup
+  // @access Private
 
-const createGroup = expressAsyncHandler(async (req, res) => {
-  const { name, groupId, channelId, type } = req.body;
+  createGroup = expressAsyncHandler(async (req, res) => {
+    const { name, groupId, channelId, type } = req.body;
 
-  const groupExist = await Group.findOne({ groupId });
-  if(groupExist) {
-    res.status(400);
-    throw new Error('Group already exists');
-  } else {
-    const channel = await Channel.findOne({ channelId: channelId })
-    if(!channel) {
+    const groupExist = await Group.findOne({ groupId });
+    if (groupExist) {
+      res.status(400);
+      throw new Error('Group already exists');
+    } else {
+      const channel = await Channel.findOne({ channelId: channelId });
+      if (!channel) {
+        res.status(400);
+        throw new Error('Channel does not exist');
+      }
+      const actualChannelId = channel._id;
+      const groupType = type == undefined ? 'public' : type;
+      const group = await Group.create({
+        name,
+        groupId,
+        channel: actualChannelId,
+        type: groupType,
+      });
+      if (group) {
+        res.status(201).json({
+          _id: group._id,
+          name: group.name,
+          groupId: group.groupId,
+          channel: group.channel,
+          type: group.type,
+        });
+      } else {
+        res.status(400);
+        throw new Error('Invalid group data');
+      }
+    }
+  });
+
+  // @desc Update the group
+  // @route PUT /api/admin/updateGroup or /api/moderator/updateGroup
+  // @access Private
+
+  updateGroup = expressAsyncHandler(async (req, res) => {
+    const { name, groupId, channelId, type } = req.body;
+    const groupExist = await Group.findOne({ groupId });
+    if (!groupExist) {
+      res.status(400);
+      throw new Error('Group does not exists');
+    } else {
+      const channel = await Channel.findOne({ channelId: channelId });
+      if (!channel) {
+        res.status(400);
+        throw new Error('Channel does not exist');
+      }
+      const actualChannelId = channel._id;
+      const groupType = type == undefined ? 'public' : type;
+      const updateGroup = await Group.findOneAndUpdate(
+        {
+          groupId: groupId,
+        },
+        {
+          name: name,
+          channel: actualChannelId,
+          type: groupType,
+        }
+      );
+      if (updateGroup) {
+        res.status(201).json({
+          _id: updateGroup._id,
+          name: name,
+          groupId: updateGroup.groupId,
+          channel: updateGroup.channel,
+          type: groupType,
+        });
+      } else {
+        res.status(400);
+        throw new Error('Invalid group data');
+      }
+    }
+  });
+
+  // @desc Get all groups
+  // @route GET /api/channels/getAllGroups
+  // @access Private to users
+
+  getAllGroups = expressAsyncHandler(async (req, res) => {
+    const { channelId } = req.params;
+    const channel = await Channel.findOne({ channelId: channelId });
+
+    if (!channel) {
       res.status(400);
       throw new Error('Channel does not exist');
-    } 
-    const actualChannelId = channel._id;
-    const groupType = type == undefined ? 'public' : type;
-    const group = await Group.create({ 
-      name, groupId,
-      channel: actualChannelId,
-      type: groupType
-    });
-    if(group) {
-      res.status(201).json({
-        _id: group._id,
-        name: group.name,
-        groupId: group.groupId,
-        channel: group.channel,
-        type: group.type
-      });
     } else {
-      res.status(400);
-      throw new Error('Invalid group data');
+      const actualChannelId = channel._id;
+      const groups = await Group.find({ channel: actualChannelId });
+      if (groups) {
+        res.status(201).json(groups);
+      } else {
+        res.status(400);
+        throw new Error('Invalid group data');
+      }
     }
-  }
-})
-
-// @desc Update the group
-// @route PUT /api/admin/updateGroup or /api/moderator/updateGroup
-// @access Private
-
-const updateGroup = expressAsyncHandler(async (req, res) => {
-  const { name, groupId, channelId, type } = req.body;
-  const groupExist = await Group.findOne({ groupId });
-  if(!groupExist) {
-    res.status(400);
-    throw new Error('Group does not exists');
-  } else {
-    const channel = await Channel.findOne({ channelId: channelId })
-    if(!channel) {
-      res.status(400);
-      throw new Error('Channel does not exist');
-    } 
-    const actualChannelId = channel._id;
-    const groupType = type == undefined ? 'public' : type;
-    const updateGroup = await Group.findOneAndUpdate({
-      groupId: groupId,
-    }, {
-      name: name,
-      channel: actualChannelId,
-      type: groupType
-    })
-    if(updateGroup) {
-      res.status(201).json({
-        _id: updateGroup._id,
-        name: name,
-        groupId: updateGroup.groupId,
-        channel: updateGroup.channel,
-        type: groupType
-      });
-    } else {
-      res.status(400);
-      throw new Error('Invalid group data');
-    }
-    }
-})
-
-// @desc Get all groups
-// @route GET /api/channels/getAllGroups
-// @access Private to users
-
-const getAllGroups = expressAsyncHandler(async (req, res) => {
-  const { channelId } = req.params;
-  const channel = await Channel.findOne({ channelId: channelId })
-
-  if(!channel) {
-    res.status(400);
-    throw new Error('Channel does not exist');
-  } else {
-    const actualChannelId = channel._id;
-    const groups = await Group.find({ channel: actualChannelId });
-    if(groups) {
-      res.status(201).json(groups);
-    } else {
-      res.status(400);
-      throw new Error('Invalid group data');
-    }
-  }
-})
-
-export {
-  createGroup,
-  updateGroup,
-  getAllGroups
+  });
 }
+
+export default new GroupController();
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,138 +1,139 @@
-import expressAsyncHandler from "express-async-handler";
-import User from "../models/user.model.js";
-import Channel from "../models/channel.model.js";
-import generateToken from "../utils/generateToken.js";
+import expressAsyncHandler from 'express-async-handler';
+import User from '../models/user.model.js';
+import Channel from '../models/channel.model.js';
+import generateToken from '../utils/generateToken.js';
 
-// @desc    Register a new user
-// @route   POST /api/users/register
-// @access  Public
+class UserController {
+  // @desc    Register a new user
+  // @route   POST /api/users/register
+  // @access  Public
 
-const registerUser = expressAsyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  registerUser = expressAsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
 
-  const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email });
 
-  if (userExists) {
-    res.status(400);
-    throw new Error('User already exists');
-  }
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
 
-  const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password });
 
-  if (user) {
-    generateToken(res, user._id);
+    if (user) {
+      generateToken(res, user._id);
 
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    });
-  } else {
-    res.status(400);
-    throw new Error('Invalid user data');
-  }
-});
-
-// @desc    Login user
-// @route   POST /api/users/login
-// @access  Public
-
-const loginUser = expressAsyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  // console.log(email, password)
-
-  const user = await User.findOne({ email });
-
-  const matchPassword = await user.matchPassword(password);
-  if (user && matchPassword) {
-    generateToken(res, user._id);
-
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    });
-  } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
-  }
-});
-
-// @desc   Logout user
-// @route  POST /api/users/logout
-// @access Private
-
-const logoutUser = expressAsyncHandler(async (req, res) => {
-  res.cookie('jwt', '', {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV !== 'development' ? 'none' : 'lax',
-    expires: new Date(0)
-  }).send({
-    message: 'Logged out',
-    success: true
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
   });
-})
 
-// @desc   Get all users
-// @route  GET /api/users
-// @access Public
+  // @desc    Login user
+  // @route   POST /api/users/login
+  // @access  Public
 
-const getAllUsers = expressAsyncHandler(async (req, res) => {
-  const users = await User.find({ role: 'user' }).populate('channels.channelId', 'channelId name description imageUrl');
-  if(users) {
-    res.status(200).json(users)
-  } else {
-    res.status(404);
-    throw new Error('No users found');
-  }
-})
+  loginUser = expressAsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    // console.log(email, password)
 
-// @desc   Get user by id
-// @route  GET /api/users/:id
-// @access Private
+    const user = await User.findOne({ email });
 
-const getUserById = expressAsyncHandler(async (req, res) => {
-  const { userId } = req.params;
-  const user = await User.findById(userId).populate('channels.channelId', 'channelId name description imageUrl');
+    const matchPassword = await user.matchPassword(password);
+    if (user && matchPassword) {
+      generateToken(res, user._id);
 
-  if(user) {
-    res.status(200).json(user)
-  } else {
-    res.status(404);
-    throw new Error('No user found');
-  }
-})
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(401);
+      throw new Error('Invalid email or password');
+    }
+  });
 
-// @desc  Is user in channel
-// @route GET /api/users/isUserInChannel/:channelId
-// @access Private
+  // @desc   Logout user
+  // @route  POST /api/users/logout
+  // @access Private
 
-const isUserInChannel = expressAsyncHandler(async (req, res) => {
-  const { channelId } = req.params;
-  const { _id } = req.user;
+  logoutUser = expressAsyncHandler(async (req, res) => {
+    res
+      .cookie('jwt', '', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV !== 'development' ? 'none' : 'lax',
+        expires: new Date(0),
+      })
+      .send({
+        message: 'Logged out',
+        success: true,
+      });
+  });
 
-  const channel = await Channel.findOne({ channelId })
-  if(!channel) {
-    res.status(404);
-    throw new Error('Channel not found');
-  }
+  // @desc   Get all users
+  // @route  GET /api/users
+  // @access Public
 
-  const user = await User.findOne({ _id, 'channels.channelId': channel._id });
-  if(user) {
-    res.status(200).json({ isInChannel: true })
-  } else {
-    res.status(200).json({ isInChannel: false })
-  }
+  getAllUsers = expressAsyncHandler(async (req, res) => {
+    const users = await User.find({ role: 'user' }).populate(
+      'channels.channelId',
+      'channelId name description imageUrl'
+    );
+    if (users) {
+      res.status(200).json(users);
+    } else {
+      res.status(404);
+      throw new Error('No users found');
+    }
+  });
 
-})
+  // @desc   Get user by id
+  // @route  GET /api/users/:id
+  // @access Private
 
+  getUserById = expressAsyncHandler(async (req, res) => {
+    const { userId } = req.params;
+    const user = await User.findById(userId).populate(
+      'channels.channelId',
+      'channelId name description imageUrl'
+    );
 
-export {
-  registerUser,
-  loginUser,
-  getAllUsers,
-  getUserById,
-  isUserInChannel,
-  logoutUser
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404);
+      throw new Error('No user found');
+    }
+  });
+
+  // @desc  Is user in channel
+  // @route GET /api/users/isUserInChannel/:channelId
+  // @access Private
+
+  isUserInChannel = expressAsyncHandler(async (req, res) => {
+    const { channelId } = req.params;
+    const { _id } = req.user;
+
+    const channel = await Channel.findOne({ channelId });
+    if (!channel) {
+      res.status(404);
+      throw new Error('Channel not found');
+    }
+
+    const user = await User.findOne({ _id, 'channels.channelId': channel._id });
+    if (user) {
+      res.status(200).json({ isInChannel: true });
+    } else {
+      res.status(200).json({ isInChannel: false });
+    }
+  });
 }
+
+export default new UserController();
